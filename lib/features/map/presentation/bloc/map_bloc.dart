a9f8186f721e8c787f665d82d808b1ab48fab702
Dmitriy Simagin <38374223+Disim@users.noreply.github.com>
2025-07-30 13:20:58 +0500

    Дополнил комментариями и кейсом со стримом

--- lib/features/map/presentation/bloc/map_bloc.dart
+++ lib/features/map/presentation/bloc/map_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
@@ -17,16 +19,23 @@ class MapBloc extends Bloc<MapEvent, MapState> {
 
   final listener = MapObjectTapListenerImpl();
 
+  final StreamController<int> _streamController = StreamController<int>();
+  Stream<int> get streamInt => _streamController.stream;
+
   void _mapLoadEvent(MapLoadEvent event, Emitter<MapState> emit) {
     final imageProvider = img.ImageProvider.fromImageProvider(
       const AssetImage("assets/station-enable.png"),
     );
+
+    /// Если нажать на маркер, то BottomSheet не откроется сразу, а только
+    /// после повторного нажатия, либо после взаимодействия с картой.
     event.mapWindow.map.mapObjects.addPlacemark()
       ..geometry = const Point(latitude: 55.751225, longitude: 37.62954)
       ..setIcon(imageProvider)
       ..addTapListener(listener)
       ..userData = () {
         add(MapOpenBottomSheetEvent());
+        _streamController.add(0);
       };
     emit(MapLoaded(event.mapWindow));
   }
@@ -35,7 +44,6 @@ class MapBloc extends Bloc<MapEvent, MapState> {
     if (state is MapLoaded) {
       final currentState = state as MapLoaded;
       emit(currentState.copyWith(openSheet: true));
-      emit(currentState.copyWith(openSheet: false));
     }
   }
 
--- lib/features/map/presentation/screens/map.dart
+++ lib/features/map/presentation/screens/map.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ya_map_bug/features/map/map.dart';
@@ -11,9 +13,34 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  StreamSubscription<int>? _streamSubscription;
+
+  @override
+  void initState() {
+    _streamSubscription = context.read<MapBloc>().streamInt.listen((data) {
+      /// После нажатия на маркер, данные отобразятся только после
+      /// взаимодействия с картой, хотя должны отображаться сразу.
+      print('Has data in stream: $data');
+    });
+    super.initState();
+  }
+
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /// Если вызвать событие через ui - BottomSheet откроется сразу.
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => context.read<MapBloc>().add(MapOpenBottomSheetEvent()),
+        label: const Text("Bottom Sheet откроется сразу"),
+      ),
+
+      /// После нажатия на маркер, BlocListener увидит MapOpenBottomSheetEvent
+      /// только после взаимодействия с картой, хотя должен сразу.
       body: BlocListener<MapBloc, MapState>(
         listener: (context, state) async {
           if (state is MapLoaded) {
